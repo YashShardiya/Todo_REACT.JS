6a56bc812fad1047656a7ec761561e5f18a3684c
yash <yashshardiya2@gmail.com>
2022-03-23 14:09:13 +0530

    1

---
+++ src/notes.js
@@ -0,0 +1,45 @@
+const contacts = [{
+    name: "Beyonce",
+    phone: "+123 456 789",
+    email: "b@beyonce.com"
+  },
+  {
+    name: "Jack Bauer",
+    phone: "+987 654 321",
+    email: "jack@nowhere.com"
+  },
+  {
+    name: "Chuck Norris",
+    phone: "+918 372 574",
+    email: "gmail@chucknorris.com"
+  }
+];
+
+export default notes;
+
+var numbers = [3, 56, 2, 48, 5];
+
+
+
+
+
+var Anonomus = function (x){
+  return x*x
+}
+
+
+
+// function creation
+function Square(x){
+  return x*x
+}
+
+// calling of function
+Square(5);
+
+
+
+
+
+
+
